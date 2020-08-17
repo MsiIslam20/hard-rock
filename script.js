4dd4d1b29f38482f5225ae478d26889b28718df3
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-17 12:49:49 +0600

    updates

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-// fetch('https://api.lyrics.ovh/suggest/baby')
-//   .then(response => response.json())
-//   .then(json => getResult(json))
+document.getElementById("searchBth").addEventListener("click", function(){
+  const getValue = document.getElementById("getValue").value;
+  fetchIssues(getValue)
+})
 
-//   const getResult = (data) => {
-//     for (let i = 0; i < 10; i++) {
-//       const element = data.data[i];
-//       const ul = document.getElementsByClassName("artist_name");
-//       // const li = document.createElement("li");
-//       ul.innerText = element.album.title;
-//       // ul.appendChild(li)
-      
-//     }  
-//   }
 
-const fetchIssues = () => {
 
-  fetch('https://api.lyrics.ovh/suggest/patola')
+const fetchIssues = (getValue) => {
+
+  fetch(`https://api.lyrics.ovh/suggest/${getValue}`)
   .then(response => response.json())
   .then(json => getResult(json))
 
@@ -39,20 +31,4 @@ const fetchIssues = () => {
                                 </div>`
     }  
   }
-
-  // for (var i = 0; i < issues.length; i++) {
-  //   const {id, description, severity, assignedTo, status} = issues[i];
-
-  //   issuesList.innerHTML +=   `<div class="well">
-  //                             <h6>Issue ID: ${id} </h6>
-  //                             <p><span class="label label-info"> ${status} </span></p>
-  //                             <h3> ${description} </h3>
-  //                             <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
-  //                             <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-  //                             <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
-  //                             <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
-  //                             </div>`;
-  // }
 }
-
-fetchIssues()
