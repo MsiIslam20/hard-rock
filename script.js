2317cac61411a78ab338bbcbb54316b9712c6401
MsiIslam20 <mohammadsaidul2k19@gmail.com>
2020-08-17 11:31:49 +0600

    updates

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-fetch('https://api.lyrics.ovh/CalvinHarris/summer')
+fetch('https://api.lyrics.ovh/suggest/baby')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(json => getResult(json))
+
+  const getResult = (data) => {
+    for (let i = 0; i < 10; i++) {
+      const element = data.data[i];
+      const ul = document.getElementsByClassName("artist_name");
+      // const li = document.createElement("li");
+      ul.innerText = element.album.title;
+      // ul.appendChild(li)
+      
+    }
+    
+  }
